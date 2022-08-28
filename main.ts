a7a3dc7f5375c8af8d5cb62fa86aceb7c53bf1ca
bumpy09 <101124139+bumpy09@users.noreply.github.com>
2022-08-28 17:37:16 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 basic.forever(function () {
     if (edubitIrBit.isIrSensorTriggered()) {
-        edubitMotors.runMotor(MotorChannel.M1, MotorDirection.Backward, 1)
         edubitRgbBit.showColor(0x000000)
+        basic.pause(randint(0, 10))
         edubitRgbBit.showColor(0x999999)
+        basic.pause(randint(0, 10))
         edubitRgbBit.showColor(0xffffff)
         basic.showString(" Wow get lighter and lighter")
+        // !
         basic.showLeds(`
             . . . . .
             . # . # .
